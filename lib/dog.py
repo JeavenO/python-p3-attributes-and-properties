e44e35512df40d7188fa7012ac5ca1ca35c4dd4f
Jeaven Onyango <jeaveno123@gmail.com>
2025-11-30 13:52:44 +0300

    Add Dog and Person classes with validated properties

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,44 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
+approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei",
+                   "Beagle", "French Bulldog", "Pug", "Pointer"]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Mutt"):
+        self._name = None
+        self._breed = None
+
+        # Set name first
+        self.name = name
+
+        # Only set breed if it's in approved breeds
+        if breed in approved_breeds:
+            self.breed = breed
+        else:
+            # Store the invalid breed anyway so the object is consistent
+            self._breed = breed
+            # Print error only if it's not the default name error situation
+            if self._name is not None:
+                print("Breed must be in list of approved breeds.")
+
+    # NAME PROPERTY
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # BREED PROPERTY
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in approved_breeds:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,44 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    pass# lib/person.py
+
+approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC",
+                 "Production", "Legal", "Finance", "Sales",
+                 "General Management", "Research & Development", "Marketing", "Purchasing"]
+
+class Person:
+    def __init__(self, name="Person", job="Admin"):
+        self.name = name      # Use property setter
+        self.job = job        # Use property setter
+
+    # NAME PROPERTY
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # JOB PROPERTY
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+
+    # INSTANCE METHODS
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
